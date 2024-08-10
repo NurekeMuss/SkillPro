92120b4fb60c512e91462153c8ca485370a2f35c
NurekeMuss <146543627+NurekeMuss@users.noreply.github.com>
2024-08-10 14:00:27 +0600

    Deploy

--- src/components/JobPage/JobPage.tsx
+++ src/components/JobPage/JobPage.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { Header } from '../Header/header';
+import { useEffect, useState } from 'react';
 import { SearchForm } from '../Search/SearchForm';
 import './jobPage.scss';
 
--- src/components/Search/SearchForm.tsx
+++ src/components/Search/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './search.scss';
 import search from '../../images/search.png';
 import '../../App.css';
